f7ea244b11d627b68fd2b40573f7c0e8f91a9e57
Joshua <joshua.soave@gmail.com>
2023-07-23 19:44:44 -0400

    added jest test

--- controllers/books.js
+++ controllers/books.js
@@ -5,13 +5,6 @@ var bodyParser = require("body-parser");
 
 require("dotenv").config();
 const apiKey = process.env.REACT_APP_APIKEY;
-// axios
-//   .get(
-//     `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${apiKey}`
-//   )
-//   .then((response) => {
-//     console.log("response ", response);
-//   });
 
 router.get("/currentFiction", (req, res) => {
   axios
